f654f2eb5b65326c106fab46c0bc92a0353e5c17
Piotr Paśka <paska.piotrek@gmail.com>
2025-06-17 13:14:13 +0200

    feat: refactor TrackGateway to implement connection handling and improve track retrieval logic

--- src/track/track.gateway.ts
+++ src/track/track.gateway.ts
@@ -1,28 +1,51 @@
 import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
 import { TrackService } from './track.service';
-import { ConfigService } from '../config/config.service';
+//import { ConfigService } from '../config/config.service';
 import { Server } from 'socket.io';
+import { ConfigService } from 'src/config/config.service';
 
-@WebSocketGateway({
-  port: new ConfigService().getConfig().ports.tracks,
-})
-export class TrackGateway {
+@WebSocketGateway(
+  new ConfigService().getConfig().ports.tracks, // Default port if not set
+  {
+    transports: ['websocket'],
+    cors: true,
+  },
+)
+export class TrackGateway implements OnGatewayConnection, OnGatewayDisconnect {
   constructor(private readonly trackService: TrackService) {}
 
   @WebSocketServer()
   server: Server;
 
-  @SubscribeMessage('get')
-  handleMessage(): string {
-    return JSON.stringify(this.trackService.getTracksWithEntryData());
+  async handleConnection() {
+    console.info('[TrackGateway] Client connected');
+    await this.sendTracks();
   }
 
-  updateTracks() {
-    const tracks = this.trackService.getTracksWithEntryData();
-    this.server.emit('update', JSON.stringify(tracks));
+  handleDisconnect() {
+    console.info('[TrackGateway] Client disconnected');
+  }
+
+  @SubscribeMessage('get')
+  async handleGetTracks(): Promise<string> {
+    console.info('[TrackGateway] Received "get" message');
+    await this.sendTracks();
+    return 'Tracks sent';
+  }
+
+  private async sendTracks() {
+    try {
+      const tracks = await this.trackService.getTracksWithEntryData();
+      console.debug('[TrackGateway] Emitting "update" with tracks data');
+      this.server.emit('update', tracks);
+    } catch (error) {
+      console.error('[TrackGateway] Error sending tracks:', error);
+    }
   }
 }
