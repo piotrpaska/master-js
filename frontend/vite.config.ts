e39f9d0b46482cd6ad88ca42a0ecff16f58e3085
Piotr Paśka <paska.piotrek@gmail.com>
2025-08-26 14:21:27 +0200

    refactor: remove unused migration files and update Docker configurations for backend and frontend services

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,4 +10,14 @@ export default defineConfig({
       '@': '/src',
     },
   },
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: 'http://0.0.0.0:8080',
+  },
 });
